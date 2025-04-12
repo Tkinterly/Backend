adbc0dcb75d677e570eeae8fa9a9bc1dc3a2eb20
Shahroz Asif <shahrozasif@proton.me>
2025-04-12 12:15:50 +0500

    feat: add support for both png and jpg image files

--- src/main/java/com/tinkerly/tinkerly/controllers/Media.java
+++ src/main/java/com/tinkerly/tinkerly/controllers/Media.java
@@ -3,25 +3,19 @@ package com.tinkerly.tinkerly.controllers;
 import com.tinkerly.tinkerly.components.EndpointResponse;
 import com.tinkerly.tinkerly.services.ImageCleanup;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.*;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.util.Base64;
 import java.util.UUID;
 
 @RestController
 @CrossOrigin
 public class Media {
 
-    private static final String UPLOAD_DIR = "/home/ubuntu/cdn/";
+    private static final String UPLOAD_DIR = "/home/spuggle/cdn/";
 
     private ImageCleanup imageCleanup;
 
@@ -30,23 +24,30 @@ public class Media {
         this.imageCleanup = imageCleanup;
     }
 
-    @PostMapping("/media/save")
-    public EndpointResponse<String> saveMedia(@RequestParam("image") MultipartFile image) throws IOException {
-        if (image.isEmpty()) {
-            return EndpointResponse.failed("Image is empty!");
-        }
+    public static class ImageRequest {
+        public String image;
+    }
 
+    @PostMapping("/media/save")
+    public EndpointResponse<String> saveMedia(@RequestBody ImageRequest imageRequest) throws IOException {
         try {
             UUID imageUUID = UUID.randomUUID();
-            String filePath = UPLOAD_DIR + imageUUID.toString();
+            String filePath = UPLOAD_DIR + imageUUID.toString() + ".png";
+            byte[] decodedBytes = Base64.getDecoder().decode(imageRequest.image);
+            InputStream inputStream = new ByteArrayInputStream(decodedBytes);
+            BufferedImage image = ImageIO.read(inputStream);
+            if (image == null) {
+                return EndpointResponse.failed("Unsupported image format or corrupted file");
+            }
 
-            Files.createDirectories(Paths.get(UPLOAD_DIR));
-            image.transferTo(new File(filePath));
+            File imageFile = new File(filePath);
+            ImageIO.write(image, "png", imageFile);
 
             imageCleanup.addImage(imageUUID.toString());
 
             return EndpointResponse.passed(imageUUID.toString());
         } catch (IOException e) {
+            System.out.println(e.getMessage());
             return EndpointResponse.failed("File upload failed!");
         }
     }
